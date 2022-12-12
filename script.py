60afccd0630412df07d13e0e7a33a1cb95452801
HuseyinFurkanMacan <huseyinfurkanmacan@hotmail.com>
2022-12-12 17:09:03 +0300

    Added View and SocialMedia func.
    We can View the current table and add special rules which blocks Facebook and Instagram.

--- script.py
+++ script.py
@@ -7,3 +7,20 @@ def Configure():
    os.system("modprobe br_netfilter")
    os.system("echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables")
    os.system("echo 'net.bridge.bridge-nf-call-iptables=1' >> /etc/sysctl.conf")
+
+def View(table='filter'):
+   table_result=iptc.easy.dump_table(table=table, ipv6=False)
+   print(table_result)
+
+def SocialMedia(table='filter'):
+   rule = iptc.Rule()
+   target = iptc.Target(rule, "DROP")
+   rule.target = target
+
+   #Facebook
+   rule.src = "157.240.238.35"
+   chain.insert_rule(rule)
+
+   #Instagram
+   rule.src = "157.240.238.174"
+   chain.insert_rule(rule)
