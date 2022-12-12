d11a4b0e9da5a348d85babf00513bc67385e0c7d
HuseyinFurkanMacan <huseyinfurkanmacan@hotmail.com>
2022-12-12 17:12:04 +0300

    Menu added.

--- script.py
+++ script.py
@@ -36,3 +36,34 @@ def Add(table='filter'):
 def Delete(table='filter'):
    table_result=iptc.easy.flush_table(table=table,ipv6=False)
    print(table_result)
+
+table=iptc.Table(iptc.Table.FILTER)
+chain=iptc.Chain(table,"FORWARD")
+iptc.Chain.set_policy(chain,policy="ACCEPT")
+
+ans=True
+while ans:
+   print ("""
+   0.Configurate.
+   1.Block Social Media.
+   2.Add a rule.
+   3.Clean the Table.
+   4.Print the Table.
+   """)
+   ans=input("What would you want to do? ")
+   if ans == "0":
+       Configure()
+   elif ans=="1":
+     print("\n Social Media Completely Blocked.")
+     SocialMedia()
+   elif ans=="2":
+     Add()
+     print("\n Rule Added.")
+   elif ans=="3":
+     Delete()
+     print("\n Table Cleaned.")
+
+   elif ans=="4":
+     View()
+   elif ans !="":
+     print("\n Not Valid Choice Try again")
